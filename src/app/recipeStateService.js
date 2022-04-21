ce4871b2fe9deb9d5bb58a298225ac9a2be48c61
Marius Albeser <68159866+Sletger@users.noreply.github.com>
2022-04-21 13:27:05 +0200

    Added summing of amounts of same ingrdients using JS find method

--- src/app/recipeStateService.js
+++ src/app/recipeStateService.js
@@ -1,6 +1,20 @@
 export function sumRecipes(recipes) {
-  return recipes.reduce((list, recipe) => {
-    //erstes Argument = Rückgabewert des vorherigen Durchlaufs
-    return [...list, ...recipe.ingredients]; // mergen der bisherigen Liste mit der neuen Liste -> zusammnegefügte List
-  }, []); // da beim ersten Durchlauf noch kein Rückgabewert des vorherigen Durchlaufs existiert wird ein leerer Array zurückgegeben
+  return recipes
+    .reduce((list, recipe) => {
+      //erstes Argument = Rückgabewert des vorherigen Durchlaufs
+      return [...list, ...recipe.ingredients]; // mergen der bisherigen Liste mit der neuen Liste -> zusammnegefügte List
+    }, []) // da beim ersten Durchlauf noch kein Rückgabewert des vorherigen Durchlaufs existiert wird ein leerer Array zurückgegeben
+    .reduce((list, ingredient) => {
+      const existingIngredient = list.find(
+        (currentIngredient) => currentIngredient.name === ingredient.name
+      );
+      //The find() method returns the value of the first element that passes a test. -> otherwise undefined
+      if (existingIngredient) {
+        existingIngredient.amount += ingredient.amount;
+      } else {
+        list.push(ingredient);
+      }
+
+      return list;
+    }, []);
 }
